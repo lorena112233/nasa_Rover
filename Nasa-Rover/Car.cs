10b17b97773affed68a8cdd56ae2cde68f97df65
lorena112233 <43742389+lorena112233@users.noreply.github.com>
2020-02-02 22:31:37 +0100

    project uploaded

---
+++ Nasa-Rover/Car.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasa_Rover
+{
+    class Car
+    {
+
+        public readonly string[] COORD = {"N", "E", "S", "W" };
+
+        // Attributes
+        private int x;
+        private int y;
+        private int facing;
+
+        // Constructor
+        public Car(string position)
+        {
+            string[] parameters = position.Split(' ');
+            this.x = Convert.ToInt16(parameters[0]);
+            this.y = Convert.ToInt16(parameters[1]);
+            this.facing = Array.IndexOf(COORD, Convert.ToString(parameters[2]));    
+
+        }
+
+        // Getters, Setters
+        public int Getx()
+        {
+
+            return x;
+
+        }
+        public void Setx(int x)
+        {
+
+            this.x = x;
+
+        }
+
+        public int Gety()
+        {
+
+            return y;
+
+        }
+        public void Sety(int y)
+        {
+
+            this.y = y;
+
+        }
+
+        public int Getfacing()
+        {
+
+            return facing;
+
+        }
+        public void Setfacing(int facing)
+        {
+
+            this.facing = facing;
+
+        }
+
+        // turning to the Rigth or to the Left
+        public void turn(string turn_order)
+        {
+
+            switch (turn_order)
+            {
+                case "L":
+                    this.facing += -1;
+                    if (this.facing == -1)
+                    {
+                        this.facing = 3;
+                    }
+                    break;
+
+                case "R":
+                    this.facing += 1;
+                    if (this.facing == 4)
+                    {
+                        this.facing = 0;
+                    }
+                    break;
+
+            }
+        }
+
+        // +1 Move ahead
+        public void move()
+        {
+            //get value of actual facing
+            int wohin = this.facing;
+
+            switch(wohin)
+            {
+                case 0: // COORD[0] = "N"
+                    this.y += 1;
+                    break;
+                case 1: // COORD[1] = "E"
+                    this.x += 1;
+                    break;
+                case 2: // COORD[2] = "S"
+                    this.y += -1;
+                    break;
+                case 3: // COORD[0] = "W"
+                    this.x += -1;
+                    break;
+
+            };
+
+        }
+
+        //shows actual position of the Car
+        public string actual_position()
+        {
+            // return actual position + (messagge)
+            string position = (this.x + " " + this.y + " " + this.facing);
+            if(this.x < 0 || this.y < 0)
+            {
+                return (position + " Rover lost in Mars//Out of Range");
+            }
+            return (position + " Successfully arrived");
+        }
+
+
+    }
+}
---
+++ Nasa-Rover/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Nasa_Rover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // save max value (x,y) = plateau size
+            int plateau_sizeX, plateau_sizeY;
+
+            //------------------ PLATEAU -------------------------
+            // get dimensions of the plateau
+            string[] plateau = create_plateau();
+            plateau_sizeX = Convert.ToInt32(plateau[0]);
+            plateau_sizeY = Convert.ToInt32(plateau[1]);
+
+
+            //------------------ ROVER 1 -------------------------
+            // get initial position for Rover1: format integers ('1 2 2')
+            Car Rover1 = create_Rover_position(plateau_sizeX, plateau_sizeY);
+            // get input for movements and move Rover1
+            Rover_movements(Rover1);
+
+            //------------------ ROVER 2 -------------------------
+            // Creating Rover2//get initial position
+            Car Rover2 = create_Rover_position(plateau_sizeX, plateau_sizeY);
+            // get input for movements and move Rover2
+            Rover_movements(Rover2);
+
+            Console.WriteLine("Rover1: " + Rover1.actual_position());
+            Console.WriteLine("Rover2: " + Rover2.actual_position());
+            Console.ReadLine();
+        }
+
+        public static string[] create_plateau()
+        {
+            string size;
+      
+            // input: size for the plateau must be between 1-9. Ex: 5 9
+            Regex regex_size = new Regex(@"^[1-9][ ][1-9]$");
+
+            // get plateau's size
+            do
+            {
+                Console.WriteLine("Introduce the size of the plateau:");
+                size = Console.ReadLine();
+
+            } while (regex_size.IsMatch(size) != true);
+
+            string[] plateau = size.Split(' ');
+
+            return plateau;
+        }
+
+
+        // create Car object
+        public static Car create_Rover_position(int plateau_sizeX, int plateau_sizeY)
+        {
+            string inicialPosition;
+
+            // input: initial position format (3 digits: x, y, facing)
+            Regex regex_position = new Regex(@"^[0-9][ ][0-9][ ][N|E|S|W]$");
+
+            do
+            {
+                Console.WriteLine("Introduce the initial position (format: '1 2 S') ");
+                inicialPosition = (Console.ReadLine()).ToUpper();
+
+
+            } while ( regex_position.IsMatch(inicialPosition) != true || verify_input(inicialPosition, plateau_sizeX, plateau_sizeY) != true );
+
+
+            Console.WriteLine("inicialPosition  " + inicialPosition);
+
+            // creating Rover1:
+            Car Rover = new Car(inicialPosition);
+
+            return Rover;
+        }
+
+        // get parameters for movements
+        public static void Rover_movements(Car Rover)
+        {
+            string movements_char;
+
+            // input: movements format (valid: "M" or "L" or "R" + minimun 1 character)
+            Regex regex_movements = new Regex(@"^(M|L|R){1,}$");
+
+            do
+            {
+                Console.WriteLine("Introduce the secuence of movements withous spaces in between: (format: 'LML') ");
+                movements_char = (Console.ReadLine().ToUpper());
+            } while (regex_movements.IsMatch(movements_char) != true);
+
+            foreach (char m in movements_char)
+            {
+                string movement = Convert.ToString(m);
+                getValues(Rover, movement);
+
+            }
+        }
+
+        // update object values based on input parameters
+        public static void getValues(Car Rover, string movement)
+        {
+            switch (movement)
+            {
+                case "L":
+                case "R":
+                    Rover.turn(movement);
+                    //Console.WriteLine(movement + "--> Rover.actual_position_" + Rover.actual_position());
+                    //Console.ReadLine();
+                    break;
+
+                case "M":
+                    Rover.move();
+                    //Console.WriteLine(movement + "--> Rover.actual_position_" + Rover.actual_position());
+                    //Console.ReadLine();
+                    break;
+            }
+        }
+
+        public static bool verify_input(string input_text, int plateau_sizeX, int plateau_sizeY)
+        {
+
+            string[] parameters = input_text.Split(' ');
+
+            if (parameters.Length != 3)
+            {
+                Console.WriteLine("3 parameters needed ( format: 0 0 N )");
+                return false;
+            }
+            else
+            {
+                int x = Convert.ToInt16(parameters[0]);
+                int y = Convert.ToInt16(parameters[1]);
+                if(x > plateau_sizeX || y > plateau_sizeY)
+                {
+                    Console.WriteLine(" Out of Range / The Rover will land out of the plateau. Please set its initial position once again: ");
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+
+            }
+
+
+        }
+        
+
+
+
+
+    }
+}
